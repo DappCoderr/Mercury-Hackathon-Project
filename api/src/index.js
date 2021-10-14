a94c2f9599c914ff361a2dd63ea8513e2f0f7df5
shweta-codeprism <kumar.mimi@gmail.com>
2021-10-14 23:38:51 +0530

    Api set up

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,41 @@
+import initApp from "./app";
+import initDB from "./db";
+import { Users } from "./models/users";
 
+import UsersService from "./services/usersService";
+
+async function run() {
+  // const config = getConfig(envVars);
+  const db = initDB({});
+
+  // Make sure to disconnect from DB when exiting the process
+  process.on("SIGTERM", () => {
+    db.destroy().then(() => {
+      process.exit(0);
+    });
+  });
+
+  // Run all database migrations
+  await db.migrate.latest();
+
+  const startAPIServer = () => {
+    console.log("Starting API server ....");
+
+    const usersService = new UsersService();
+
+    const app = initApp(usersService);
+
+    app.listen(3001, () => {
+      console.log(`Listening on port 3001!`);
+    });
+  };
+
+  startAPIServer();
+}
+
+const redOutput = "\x1b[31m%s\x1b[0m";
+
+run().catch(e => {
+  console.error(redOutput, e);
+  process.exit(1);
+});
